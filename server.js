bd892696b322fd3b8980ef8db5804927e9345a5d
faresdawood10-dev <faresdawood10@gmail.com>
2026-05-02 19:28:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Fadi's Clover backend is running.");
